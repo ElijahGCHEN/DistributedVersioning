f4a1bdb680a065fb39c7f2d3a225f973e5e8500a
Elijah G. CHEN <gchenam@cse.ust.hk>
2021-05-11 14:21:29 +0800

    Initial Commit

---
+++ dv/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  dv
+//
+//  Created by Elijah G. CHEN on 11/5/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
